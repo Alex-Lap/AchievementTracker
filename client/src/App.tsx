5c53aeabe2969699cca7b0f59fb18b8dc6a935ec
alexanderlapsley <alxmlap@gmail.com>
2026-01-14 17:16:40 -0500

    set up client template

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Click Leaderboard</h1>
+      <p>Frontend is running.</p>
+    </main>
+  );
+}
